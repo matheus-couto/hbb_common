c3e13e0f5b6c57527f7eac17a9854c83abd2eb79
Matheus Couto <32336253+matheus-couto@users.noreply.github.com>
2025-03-11 11:45:42 -0300

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["acesso.opcaosistemas.com"];
+pub const RS_PUB_KEY: &str = "IzEtVikyDmckyG4pn4svg04uVo4hfuIKZw53mp82XM4=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
